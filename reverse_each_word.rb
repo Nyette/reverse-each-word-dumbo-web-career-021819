2beac676a58a771105dfa7600d538553d94111fb
Antoinette Lee <antoinette.lee02@myhunter.cuny.edu>
2019-02-01 00:49:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,20 +1,20 @@
-def reverse_each_word(sentence)
-  array = sentence.split(" ")
-  array.collect do |word|
-    word.reverse
-  end
-  array.join(" ")
-end
-
 # def reverse_each_word(sentence)
-#  array_reversed_words = []
 #  array = sentence.split(" ")
-#  array.each do |word|
-#    reversed_word = word.reverse
-#    array_reversed_words << reversed_word
+#  array.collect do |word|
+#    word.reverse
 #  end
-#  array_reversed_words.join(" ")
+#  array.join(" ")
 # end
+
+def reverse_each_word(sentence)
+  array_reversed_words = []
+  array = sentence.split(" ")
+  array.each do |word|
+  reversed_word = word.reverse
+    array_reversed_words << reversed_word
+  end
+  array_reversed_words.join(" ")
+end
 
 
 
